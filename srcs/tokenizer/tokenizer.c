0c1aca6f29cdd1a77b046b6f170a21ecccc5bedb
Phalor <niguinti@e1r8p4.42.fr>
2019-10-02 01:39:19 +0200

    tokenizer

--- srcs/tokenizer/tokenizer.c
+++ srcs/tokenizer/tokenizer.c
@@ -6,8 +6,45 @@
 /*   By: niguinti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 05:04:06 by niguinti          #+#    #+#             */
-/*   Updated: 2019/10/01 05:04:21 by niguinti         ###   ########.fr       */
+/*   Updated: 2019/10/02 01:38:16 by niguinti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "tokenizer.h"
 
+void	tokenizer(char *s)
+{
+	int i = 0;
+	t_chr_class chr_class = 0;
+	t_toktype token;
+	int anchor = 0;
+
+	while (s[i])
+	{
+		chr_class = get_chr_class[(unsigned char)s[i]];
+		token = get_tok_type[chr_class];
+		if (token)
+		{
+			while (s[i] && get_chr_class[(unsigned char)s[i]] == chr_class)
+			{
+				chr_class = get_chr_class[(unsigned char)s[i]];
+				printf("[%s, '%c', %d]\n", DEBUG_CHR[chr_class], s[i], anchor);
+				anchor++;
+				i++;
+			}
+			printf("{%s, \"%.*s\"}\n", DEBUG_TOKEN[token], anchor, s + (i - anchor));
+			anchor = 0;
+			if (chr_class == CHR_SP)
+				i++;
+		}
+		else
+			i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	tokenizer(av[1]);
+
+	return (0);
+}
--- srcs/tokenizer/tokenizer.h
+++ srcs/tokenizer/tokenizer.h
@@ -6,11 +6,11 @@
 /*   By: niguinti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 05:04:16 by niguinti          #+#    #+#             */
-/*   Updated: 2019/10/01 22:35:30 by niguinti         ###   ########.fr       */
+/*   Updated: 2019/10/02 01:33:08 by niguinti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lexer.h"
+#include <stdio.h>
 
 typedef enum	e_toktype {
 	TOK_SP,
@@ -18,7 +18,11 @@ typedef enum	e_toktype {
 	TOK_VALIDATOR,
 	TOK_OPERATOR,
 	TOK_NEWLINE,
-	TOK_ESCAPE
+	TOK_REDIRECTION,
+	TOK_ESCAPE,
+	TOK_SQUOTE,
+	TOK_DQUOTE,
+	TOK_PIPE
 } t_toktype;
 
 typedef enum	e_chr_class {
@@ -29,11 +33,12 @@ typedef enum	e_chr_class {
 	CHR_OPERATOR,
 	CHR_PIPE,
 	CHR_SQUOTE,
-	CHR_DQUOTE
+	CHR_DQUOTE,
+	CHR_REDIRECTION
 } t_chr_class;
 
 
-t_toktype		get_current_tok_type[256] = {
+t_chr_class		get_chr_class[256] = {
 	[' '] = CHR_SP,
 	['\t'] = CHR_SP,
 	[';'] = CHR_VALIDATOR,
@@ -53,6 +58,42 @@ t_toktype		get_current_tok_type[256] = {
 	['\''] = CHR_SQUOTE,
 	['"'] = CHR_DQUOTE,
 	
-	['='] = CHR_OPERATOR,
-	['&'] = CHR_OPERATOR
-}
+	['='] = CHR_OPERATOR
+};
+
+t_toktype	get_tok_type[CHR_REDIRECTION + 1] = {
+	[CHR_SP] = TOK_SP,
+	[CHR_WORD] = TOK_WORD,
+	[CHR_VALIDATOR] = TOK_VALIDATOR,
+	[CHR_ESCAPE] = TOK_ESCAPE,
+	[CHR_REDIRECTION] = TOK_REDIRECTION,
+	[CHR_PIPE] = TOK_PIPE,
+	[CHR_SQUOTE] = TOK_SQUOTE,
+	[CHR_DQUOTE] = TOK_DQUOTE,
+	[CHR_OPERATOR] = TOK_OPERATOR
+};
+
+const char	DEBUG_TOKEN[TOK_PIPE + 1][30] = {
+	[TOK_SP] = "TOK_SP",
+	[TOK_WORD] = "TOK_WORD",
+	[TOK_VALIDATOR] = "TOK_VALIDATOR",
+	[TOK_OPERATOR] = "TOK_OPERATOR",
+	[TOK_NEWLINE] = "TOK_NEWLINE",
+	[TOK_REDIRECTION] = "TOK_REDIRECTION",
+	[TOK_ESCAPE] = "TOK_ESCAPE",
+	[TOK_SQUOTE] = "TOK_SQUOTE",
+	[TOK_DQUOTE] = "TOK_DQUOTE",
+	[TOK_PIPE] = "TOK_PIPE"
+};
+
+const char	DEBUG_CHR[CHR_REDIRECTION + 1][30] = {
+	[CHR_SP] = "CHR_SP",
+	[CHR_WORD] = "CHR_WORD",
+	[CHR_VALIDATOR] = "CHR_VALIDATOR",
+	[CHR_OPERATOR] = "CHR_OPERATOR",
+	[CHR_ESCAPE] = "CHR_ESCAPE",
+	[CHR_PIPE] = "CHR_PIPE",
+	[CHR_SQUOTE] = "CHR_SQUOTE",
+	[CHR_DQUOTE] = "CHR_DQUOTE",
+	[CHR_REDIRECTION] = "CHR_REDIRECTION"
+};
