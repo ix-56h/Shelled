d82768c78b2168a2fb966af6f8a896909a30ac3b
Phalor <niguinti@e1r8p4.42.fr>
2019-10-02 06:47:21 +0200

    addign tokenizer rules for chr_class depending of current state of token and adding supporting of sequences with signle quotes for the test

--- srcs/tokenizer/tokenizer.c
+++ srcs/tokenizer/tokenizer.c
@@ -6,39 +6,108 @@
 /*   By: niguinti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 05:04:06 by niguinti          #+#    #+#             */
-/*   Updated: 2019/10/02 01:38:16 by niguinti         ###   ########.fr       */
+/*   Updated: 2019/10/02 06:46:03 by niguinti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tokenizer.h"
+#include <stdlib.h>
+#include <string.h>
+
+int			is_opening_class(t_chr_class chr_class)
+{
+	if (chr_class == CHR_SQUOTE)
+		return (1);
+	return (0);
+}
+
+t_tokens	*save_token(char *s, int anchor, t_toktype toktype)
+{
+	t_tokens	*new;
+
+	if (!(new = malloc(sizeof(t_tokens)))
+		|| !(new->token_literal = malloc(sizeof(char) * (anchor + 1))))
+		return (NULL);
+	new->token = toktype;
+	//a remplacer par ft_strncpy
+	strncpy(s, new->token_literal, anchor);
+	new->token_literal[anchor] = 0;
+	new->next = NULL;
+	return (new);
+}
+
+void	ignore_chr_class(char *s, int *i, t_chr_class chr_class)
+{
+	while (s[*i] && get_chr_class[(unsigned char)s[*i]] == chr_class)
+	{
+		(*i)++;
+	}
+}
+
+t_tokens	*get_sequence_token(char *s, int *i, t_toktype toktype, t_chr_class origin_class)
+{
+	t_chr_class		chr_class = 0;
+	t_chr_class		prev_class = 0;
+	int				anchor = 0;
+
+	while (s[*i] && ((chr_class = get_chr_class[s[*i]]) != origin_class || prev_class == CHR_ESCAPE))
+	{
+		prev_class = chr_class;
+		(chr_class == CHR_ESCAPE) ? (*i)++ : 0;
+		//printf("[%s, '%c', %d]\n", DEBUG_CHR[chr_class], s[*i], anchor);
+		anchor++;
+		(*i)++;
+	}
+	if (chr_class != origin_class)
+	{
+		printf("ERROR : Need to close sequence\n");
+		exit (0);
+	}
+	printf("{%s, \"%.*s\"}\n", DEBUG_TOKEN[toktype], anchor, s + (*i - anchor));
+	return (save_token(s + (*i - anchor), anchor, toktype));
+}
+
+t_tokens	*get_token(char *s, int *i, t_toktype toktype, t_chr_class prev_class)
+{
+	t_chr_class chr_class = 0;
+	int anchor = 0;
+	
+	chr_class = get_chr_class[(unsigned char)s[*i]];
+	while (s[*i] &&
+		  (token_chr_rules[toktype][(chr_class = get_chr_class[(unsigned char)s[*i]])] || prev_class == CHR_ESCAPE))
+	{
+		(chr_class == CHR_ESCAPE) ? (*i)++ : 0;
+		prev_class = chr_class;
+		//printf("[%s, '%c', %d]\n", DEBUG_CHR[chr_class], s[*i], anchor);
+		anchor++;
+		(*i)++;
+	}
+	printf("{%s, \"%.*s\"}\n", DEBUG_TOKEN[toktype], anchor, s + (*i - anchor));
+	return (save_token(s + (*i - anchor), anchor, toktype));
+}
 
 void	tokenizer(char *s)
 {
 	int i = 0;
 	t_chr_class chr_class = 0;
-	t_toktype token;
-	int anchor = 0;
-
+	
 	while (s[i])
 	{
 		chr_class = get_chr_class[(unsigned char)s[i]];
-		token = get_tok_type[chr_class];
-		if (token)
+		if (chr_class == CHR_SP)
 		{
-			while (s[i] && get_chr_class[(unsigned char)s[i]] == chr_class)
-			{
-				chr_class = get_chr_class[(unsigned char)s[i]];
-				printf("[%s, '%c', %d]\n", DEBUG_CHR[chr_class], s[i], anchor);
-				anchor++;
-				i++;
-			}
-			printf("{%s, \"%.*s\"}\n", DEBUG_TOKEN[token], anchor, s + (i - anchor));
-			anchor = 0;
-			if (chr_class == CHR_SP)
-				i++;
+			ignore_chr_class(s, &i, CHR_SP);
+			continue ;
 		}
-		else
+		if (is_opening_class(chr_class))
+		{
 			i++;
+			if (!get_sequence_token(s, &i, get_tok_type[chr_class], chr_class))
+				exit (0);
+			i++;
+		}
+		else if (!get_token(s, &i, get_tok_type[chr_class], chr_class))
+			exit (0);
 	}
 }
 
--- srcs/tokenizer/tokenizer.h
+++ srcs/tokenizer/tokenizer.h
@@ -6,7 +6,7 @@
 /*   By: niguinti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 05:04:16 by niguinti          #+#    #+#             */
-/*   Updated: 2019/10/02 01:33:08 by niguinti         ###   ########.fr       */
+/*   Updated: 2019/10/02 05:34:38 by niguinti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ typedef enum	e_toktype {
 typedef enum	e_chr_class {
 	CHR_SP,
 	CHR_WORD,
+	CHR_DIGIT,
 	CHR_VALIDATOR,
 	CHR_ESCAPE,
 	CHR_OPERATOR,
@@ -37,6 +38,11 @@ typedef enum	e_chr_class {
 	CHR_REDIRECTION
 } t_chr_class;
 
+typedef struct	s_tokens {
+	t_toktype		token;
+	char			*token_literal;
+	struct s_tokens	*next;
+}				t_tokens;
 
 t_chr_class		get_chr_class[256] = {
 	[' '] = CHR_SP,
@@ -46,7 +52,7 @@ t_chr_class		get_chr_class[256] = {
 
 	['A' ... 'Z'] = CHR_WORD,
 	['a' ... 'z'] = CHR_WORD,
-	['0' ... '9'] = CHR_WORD,
+	['0' ... '9'] = CHR_DIGIT,
 
 	['\\'] = CHR_ESCAPE,
 	
@@ -59,6 +65,34 @@ t_chr_class		get_chr_class[256] = {
 	['"'] = CHR_DQUOTE,
 	
 	['='] = CHR_OPERATOR
+};
+
+t_toktype	token_chr_rules[TOK_PIPE + 1][CHR_REDIRECTION + 1] = {
+	[TOK_SP] = {[CHR_SP] = 0,},
+	[TOK_WORD] = {[CHR_WORD] = 1, [CHR_DIGIT] = 1},
+	[TOK_SQUOTE] = {
+		[CHR_WORD] = 1,
+		[CHR_SP] = 1,
+		[CHR_DQUOTE] = 1,
+		[CHR_VALIDATOR] = 1,
+		[CHR_ESCAPE] = 1,
+		[CHR_REDIRECTION] = 1,
+		[CHR_PIPE] = 1,
+		[CHR_OPERATOR] = 1,
+		[CHR_DIGIT] = 1
+	},
+	[TOK_DQUOTE] = {
+		[CHR_WORD] = 1,
+		[CHR_SP] = 1,
+		[CHR_SQUOTE] = 1,
+		[CHR_VALIDATOR] = 1,
+		[CHR_ESCAPE] = 1,
+		[CHR_REDIRECTION] = 1,
+		[CHR_PIPE] = 1,
+		[CHR_OPERATOR] = 1,
+		[CHR_DIGIT] = 1
+	},
+	[TOK_OPERATOR] = {[CHR_OPERATOR] = 1}
 };
 
 t_toktype	get_tok_type[CHR_REDIRECTION + 1] = {
