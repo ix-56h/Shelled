b7e03197ad5800e6b86731b45650f7b91e160593
Phalor <niguinti@e1r11p15.42.fr>
2019-10-19 03:01:45 +0200

    parser

---
+++ srcs/parser/parser.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niguinti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/19 02:56:03 by niguinti          #+#    #+#             */
+/*   Updated: 2019/10/19 03:01:12 by niguinti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	parse_program(void)
+{
+	// here, all parse functions "eat" the token, when whe check the token on 
+	// second and third if, the token have changed
+	//if (token == linebreak)
+	//	parse_linebreak();
+	//else
+	//	return error
+	//if ([complete_commands][token])
+	//	parse_complete_commands();
+	//if (token == linebreak)
+	//	parse_linebreak();
+	
+}
+
