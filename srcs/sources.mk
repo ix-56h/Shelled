a04a08b72cd0923a5d1aa5a7c8da4cd02e4bc031
Phalor <niguinti@e1r13p5.42.fr>
2019-12-23 01:57:11 +0100

    feature-executor: source files sorted

--- srcs/sources.mk
+++ srcs/sources.mk
@@ -6,7 +6,7 @@
 #    By: thdelmas <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/13 19:40:02 by thdelmas          #+#    #+#              #
-#    Updated: 2019/12/08 15:41:29 by thdelmas         ###   ########.fr        #
+#    Updated: 2019/12/23 01:52:57 by niguinti         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,13 +16,18 @@ C_FILES = \
 C_FILES_parser = \
 	parser.c \
 	misc.c \
-	tokenizer.c \
 	dynamic_arrays.c \
 	parse_error.c \
-	wordexp_tokenizer.c	\
-	wordexp_misc.c \
 	stack.c \
 	free_tree.c \
+
+C_FILES_tokenizer = \
+	tokenizer.c \
+	wordexp_tokenizer.c	\
+	wordexp_misc.c \
+
+C_FILES_ast_visitor = \
+
 
 # SUPPA concat loop
 C_FILES += $(foreach SUB_DIR,$(SUB_DIRS),$(addprefix $(SUB_DIR)/,$(C_FILES_$(SUB_DIR))))
