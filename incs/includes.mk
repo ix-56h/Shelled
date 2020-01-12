5bc618cb4bffe399c7276b5ece22377058d26023
akeiflin <akeiflin@student.42.fr>
2020-01-12 23:21:13 +0100

    fix-builtin: remove builtin_rules

--- incs/includes.mk
+++ incs/includes.mk
@@ -6,7 +6,7 @@
 #    By: akeiflin <akeiflin@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/04/30 14:33:36 by thdelmas          #+#    #+#              #
-#    Updated: 2020/01/12 23:01:26 by akeiflin         ###   ########.fr        #
+#    Updated: 2020/01/12 23:20:37 by akeiflin         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,6 +23,6 @@ H_FILES		=	parser.h \
 				visitor_rules.h \
 				sh.h \
 				builtins.h \
-				builtin_rules.h \
+
 # SUPPA concat loop
 #H_FILES += $(foreach SUB_DIR,$(SUB_DIRS),$(addprefix $(SUB_DIR)/,$(H_FILES_$(SUB_DIR))))
