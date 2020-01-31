db095b36a27d520a93ca2c23498bd2a4fa602ffc
Phalor <niguinti@e1r11p8.42.fr>
2020-02-01 00:46:01 +0100

    nrm-parser: fix escaping in tokenizer and refactoring, now are normed

--- srcs/tokenizer/tokenizer.c
+++ srcs/tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: akeiflin <akeiflin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/19 06:36:10 by niguinti          #+#    #+#             */
-/*   Updated: 2020/01/30 14:30:38 by niguinti         ###   ########.fr       */
+/*   Updated: 2020/02/01 00:38:34 by niguinti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,66 +33,76 @@ t_toktype	get_true_toktype(char *s, t_toktype toktype, int *i)
 	return (ret);
 }
 
-int			get_token(char *s, t_chr_class chr_class \
-			, t_lifo *stack, t_tokens *tok)
+int			get_token(char *s, t_gnt *g, t_lifo *stack)
 {
 	t_chr_class	prev_class;
 	int			anchor;
-	t_toktype	toktype;
 
-	prev_class = chr_class;
 	anchor = 0;
-	toktype = get_tok_type[prev_class];
-	if (is_special_char(chr_class, prev_class)
+	prev_class = g->chr_class;
+	if (is_special_char(prev_class, prev_class)
 		&& lex_sequence(s, &anchor, stack) == 0)
 		return (anchor);
-	while (s[anchor] && (prev_class == CHR_ESCAPE || token_chr_rules[toktype]
-			[(chr_class = get_chr_class[(unsigned char)s[anchor]])]))
+	anchor += (s[anchor] ? 1 : 0);
+	g->chr_class = get_chr_class[(unsigned char)s[anchor]];
+	while (s[anchor] && (prev_class == CHR_ESCAPE || token_chr_rules\
+[g->toktype][(g->chr_class = get_chr_class[(unsigned char)s[anchor]])]))
 	{
-		if (is_special_char(chr_class, prev_class))
+		if (is_special_char(g->chr_class, prev_class) == 1)
 		{
 			if (!lex_sequence(s, &anchor, stack))
 				return (anchor);
 			continue;
 		}
-		prev_class = chr_class;
+		prev_class = g->chr_class;
 		anchor++;
 	}
-	((toktype == TOK_LPAREN || toktype == TOK_RPAREN) ? (anchor += 1) : 0);
-	*tok = save_token(s, anchor, toktype);
+	g->toktype == TOK_LPAREN || g->toktype == TOK_RPAREN ? (anchor += 1) : 0;
+	g->token = save_token(s, anchor, g->toktype);
 	return (anchor);
 }
 
-t_tokens	get_next_token(char *s, t_lifo*stack)
+int			init_and_post_process(char *s, t_gnt *gnt, t_lifo *stack)
 {
-	t_chr_class		chr_class = 0;
-	t_toktype		toktype = 0;
-	t_tokens		token;
-	static int		i = 0;
-
-	if (s[i] == '\0')
+	gnt->chr_class = 0;
+	if (s[gnt->i] == '\0')
 	{
-		i = 0;
-		return (save_token(s, 0, TOK_EOF));
+		gnt->i = 0;
+		gnt->token = save_token(s, 0, TOK_EOF);
+		return (1);
 	}
-	if (!(chr_class = get_chr_class[(unsigned char)s[i]]))
-		return (token_error(UNRECOGNIZED_TOKEN, stack, s[i]));
-	if (chr_class == CHR_COMMENT || chr_class == CHR_SP)
+	if (!(gnt->chr_class = get_chr_class[(unsigned char)s[gnt->i]]))
 	{
-		ignore_chr_class(s, &i, chr_class);
+		gnt->token = token_error(UNRECOGNIZED_TOKEN, stack, s[gnt->i]);
+		return (1);
+	}
+	return (0);
+}
+
+t_tokens	get_next_token(char *s, t_lifo *stack)
+{
+	static t_gnt	gnt;
+
+	if (init_and_post_process(s, &gnt, stack) == 1)
+		return (gnt.token);
+	if (gnt.chr_class == CHR_COMMENT || gnt.chr_class == CHR_SP)
+	{
+		ignore_chr_class(s, &(gnt.i), gnt.chr_class);
 		return (get_next_token(s, stack));
 	}
-	if (!(toktype = get_tok_type[chr_class]))
-		return (token_error(UNRECOGNIZED_TOKEN, stack, s[i]));
-	i += get_token(s + i, chr_class, stack, &token);
+	if (!(gnt.toktype = get_tok_type[gnt.chr_class]))
+		return (token_error(UNRECOGNIZED_TOKEN, stack, s[gnt.i]));
+	gnt.i += get_token(s + gnt.i, &gnt, stack);
 	if (!lifo_empty(stack))
 		return (token_error(0, stack, 0));
-	if (token.tok == TOK_WORD && (s[i] == '>' || s[i] == '<') && (ft_isdigits(token.data)))
-		token.tok = TOK_IO_NUMBER;
-	if (ABSTRACT_TOKEN[token.tok] && !(token.tok = get_true_toktype(token.data, token.tok, &i)))
+	if (gnt.token.tok == TOK_WORD && (s[gnt.i] == '>' || s[gnt.i] == '<') \
+		&& (ft_isdigits(gnt.token.data)))
+		gnt.token.tok = TOK_IO_NUMBER;
+	if (ABSTRACT_TOKEN[gnt.token.tok] && !(gnt.token.tok = \
+				get_true_toktype(gnt.token.data, gnt.token.tok, &(gnt.i))))
 	{
-		error_push(stack, UNRECOGNIZED_TOKEN, token.data);
+		error_push(stack, UNRECOGNIZED_TOKEN, gnt.token.data);
 		return (token_error(0, stack, 0));
 	}
-	return (token);
+	return (gnt.token);
 }
