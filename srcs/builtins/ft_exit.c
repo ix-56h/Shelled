4c8f8e4592439324ef17a844e749b1a3d0386a89
akeiflin <akeiflin@student.42.fr>
2020-02-09 17:27:32 +0100

    fix-exec: little fix

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: akeiflin <akeiflin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 06:57:57 by niguinti          #+#    #+#             */
-/*   Updated: 2020/02/09 02:28:47 by akeiflin         ###   ########.fr       */
+/*   Updated: 2020/02/09 17:27:16 by akeiflin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ int		ft_exit(char **argv, char ***tenv)
 	g_exit = 0;
 	if (argv[1])
 		g_exit = ft_atoi(argv[1]);
-	return (g_exit);
+	return (0);
 }
