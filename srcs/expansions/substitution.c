d77f1295ff5751265ca1a31e2de5f7a6b6670ac9
Phalor <niguinti@e2r4p9.42.fr>
2020-03-01 05:19:10 +0100

    feature-substitution: parse inside the substitution, need to make a function avec pipe the output of the subshell into a buffer and replace by the redirected output

--- srcs/expansions/substitution.c
+++ srcs/expansions/substitution.c
@@ -6,7 +6,7 @@
 /*   By: niguinti <0x00fi@protonmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 16:14:52 by niguinti          #+#    #+#             */
-/*   Updated: 2020/02/25 17:19:05 by niguinti         ###   ########.fr       */
+/*   Updated: 2020/03/01 05:18:00 by niguinti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,20 @@
 
 char	*exec_substitution(char *tmp)
 {
-	//t_sh		sh;
-	(void)tmp;
+	t_sh		sh;
+	
+	if (!(sh.stack.errors = lifo_creator(20, sizeof(t_staterror))))
+		return (0);
+	sh.node = NULL;
+	sh.input = tmp;
+	sh.tok = get_next_token(sh.input, sh.stack.errors);
+	lifo_empty(sh.stack.errors) ? sh.node = parse_command(&sh) : 0;
+	if (!lifo_empty(sh.stack.errors))
+	{
+		print_stack_errors(sh.stack.errors, &sh.tok);
+		return (ft_strdup("FAILED"));
+	}
+	tree_draw(sh.node);
 	return (ft_strdup("COMMAND"));
 }
 
@@ -36,9 +48,9 @@ char	*process_substitution(size_t *i, char *word)
 		y++;
 	if (word[y] == ')')
 	{
-		if (!(nw = malloc((sizeof(char) * y) + 1)))
+		if (!(nw = ft_memalloc((sizeof(char) * y))))
 			exit(1);
-		ft_strncpy(nw, word + *i, y);
+		ft_strncpy(nw, word + *i, y - 1);
 		tmp = exec_substitution(nw);
 		free(nw);
 		word[*i - 1] = 0;
